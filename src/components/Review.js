1b571892b59c040435c98457e65763851f5af311
Eunyoung-Jo <82076033+Eunyoung-Jo@users.noreply.github.com>
2023-09-24 18:13:57 +0900

    Update Review.js

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,32 @@
-// import React from "react";
-// import
+import Container from 'react-bootstrap/Container';
+import Nav from "./components/Nav.js";
+import ListGroup from 'react-bootstrap/ListGroup';
 
-// function Review({ name }) {
-//     return <h1>{name}</h1>;
-// }
 
+function NumberedExample() {
+    return (
+        <ListGroup as="ol" numbered>
+            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+        </ListGroup>
+    );
+}
+
+
+function ActiveExample() {
+    return (
+        <ListGroup as="ul">
+            <ListGroup.Item as="li" active>
+                Cras justo odio
+            </ListGroup.Item>
+            <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item as="li" disabled>
+                Morbi leo risus
+            </ListGroup.Item>
+            <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
+        </ListGroup>
+    );
+}
+
+export default Review;
